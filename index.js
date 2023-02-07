b178f49f13b79bf80553f7a4bd63e22c3db8bef4
Spellmaker <mbts.org@googlemail.com>
2023-02-07 13:46:55 +0100

    new features

--- index.js
+++ index.js
@@ -98,11 +98,14 @@ const recipes = [
     recipe("Block of Steel", 6, "Steel Ingot", 9),
     recipe("Steel Mechanical Component", 1, "Steel Plate", 4, "Copper Ingot", 1),
     recipe("Reinforced Blast Brick", 1, "Blast Brick", 1, "Steel Plate", 1),
+    recipe("Piston", 1, "Wood Sidings", 3, "Iron Plate", 1, "Redstone", 1),
 ];
 
 function clearAll() {
     selection.splice(0, selection.length);
+    inventory.splice(0, inventory.length);
     localStorage.recipeSelection = undefined;
+    localStorage.inventory = undefined;
     console.log(selection);
     updateView();
 }
@@ -117,6 +120,14 @@ if (localStorage.recipeSelection) {
     parsed.forEach((r) => {
         selection.push(r);
     })
+}
+
+const inventory = [];
+if (localStorage.inventory) {
+    const parsed = JSON.parse(localStorage.inventory);
+    parsed.forEach((r) => {
+        inventory.push(r);
+    })
 
 }
 
@@ -124,17 +135,20 @@ const searchContents = document.getElementById("search-contents");
 const searchBox = document.getElementById("search");
 const selectionPanel = document.getElementById("selected-panel");
 const resultsPanel = document.getElementById("right-panel");
+const inventoryPanel = document.getElementById("inventory-panel");
+let showStacks = false;
 
-function decomposeRecipe(name, neededAmount) {
-    console.log("decompose " + name);
+function toggleStacks() {
+    showStacks = !showStacks;
+    updateView();
+}
+
+function decomposeRecipe(name, neededAmount, shadowInventory) {
+    // TODO
+    
     const found = recipes.find((r) => r.name == name);
     
     if (!found) {
-        console.log("fast return");
-        console.log([{
-            name: name,
-            amount: neededAmount,
-        }]);
         return [{
             name: name,
             amount: neededAmount,
@@ -142,41 +156,90 @@ function decomposeRecipe(name, neededAmount) {
     }
     let produced = 0;
     let iterations = 0;
+
+    /* check if any components are already in inventory */
+    console.log(shadowInventory);
+    const inventoryEntry = shadowInventory.find(e => e.recipe.name == name);
+    if (inventoryEntry) {
+        produced = inventoryEntry.amount;
+        inventoryEntry.amount -= neededAmount;
+        if (inventoryEntry.amount < 0) {
+            inventoryEntry.amount = 0;
+        }
+    }
+
     while (produced < neededAmount) {
         iterations++;
         produced += found.amount;
     }
-    return found.recipe.flatMap(component => decomposeRecipe(component.name, component.amount * iterations));
+
+    if (iterations <= 0) {
+        return [];
+    }
+    return found.recipe.flatMap(component => decomposeRecipe(component.name, component.amount * iterations, shadowInventory));
 }
 
-function updateAmount(id) {
-    const element = document.getElementById("input-" + id);
+function updateAmount(id, isSelection) {
+    let prefix = "selection";
+    if (!isSelection) {
+        prefix = "inventory";
+    }
+    console.log("input-" + prefix + "-" + id);
+    const element = document.getElementById("input-" + prefix + "-" + id);
+    let arrayToUpdate = selection;
+    if (!isSelection) {
+        arrayToUpdate = inventory;
+    }
 
     if (element.value == 0) {
-        selection.splice(selection.findIndex((e) => e.recipe.id == id), 1);
+        arrayToUpdate.splice(arrayToUpdate.findIndex((e) => e.recipe.id == id), 1);
     } else {
-        selection.find((e) => e.recipe.id == id).amount = element.value;
+        arrayToUpdate.find((e) => e.recipe.id == id).amount = element.value;
     }
     updateView();
 }
 
-function renderSelected(selectedElement) {
-    return "<div class='selected-element'><input type='number' id='input-"+ selectedElement.recipe.id + "' onInput='updateAmount(\"" + selectedElement.recipe.id + "\")' value='"+ selectedElement.amount +"'/>" + selectedElement.recipe.name + "</div>"
+function renderSelected(selectedElement, isSelection) {
+    let stringPart = "selection";
+    if (!isSelection) {
+        stringPart = "inventory";
+    }
+    return "<div class='"+ stringPart + "-element'><input type='number' id='input-"+ stringPart +"-"+ selectedElement.recipe.id + "' onInput='updateAmount(\"" + selectedElement.recipe.id + "\", " + isSelection + ")' value='"+ selectedElement.amount +"'/>" + selectedElement.recipe.name + "</div>"
+}
+
+function renderAmount(amount) {
+    if (showStacks) {
+        return Math.floor(amount / 64) + ":" + (amount % 64);
+    } else {
+        return amount;
+    }
 }
 
 function renderIngredient(ingredient) {
-    return "<tr><td>" + ingredient.amount + "</td><td>" + ingredient.name + "</td></tr>";
+    return "<tr><td>" + renderAmount(ingredient.amount) + "</td><td>" + ingredient.name + "</td></tr>";
+}
+
+function inventoryCopy() {
+    const result = [];
+    inventory.forEach((e) => {
+        result.push({amount: e.amount, recipe: e.recipe });
+    });
+    return result;
 }
 
 function updateView() {
     localStorage.recipeSelection = JSON.stringify(selection);
+    localStorage.inventory = JSON.stringify(inventory);
     selectionPanel.innerHTML = "";
+    inventoryPanel.innerHTML = "";
 
-    selection.forEach((e) => selectionPanel.innerHTML += renderSelected(e));
+    selection.forEach((e) => selectionPanel.innerHTML += renderSelected(e, true));
+    inventory.forEach((e) => inventoryPanel.innerHTML += renderSelected(e, false));
 
     resultsPanel.innerHTML = "";
+    const computeInventory = inventoryCopy();
 
-    const step1 = selection.flatMap((e) => decomposeRecipe(e.recipe.name, e.amount));
+    const step1 = selection.flatMap((e) => decomposeRecipe(e.recipe.name, e.amount, computeInventory));
 
     const decomposed = [];
     step1.forEach(e => {
@@ -187,22 +250,31 @@ function updateView() {
             decomposed.push(e);
         }
     });
+    decomposed.sort((a, b) => a.name.localeCompare(b.name));
 
-    console.log(decomposed);
+
     let inner = "<table><tr><td>Amount</td><td>Name</td></tr>";
 
     decomposed.forEach(d => inner += renderIngredient(d));
     inner += "</table>";
+    inner += "<input type='checkbox' onChange='toggleStacks()'>Show Stacks";
     resultsPanel.innerHTML = inner;
 }
 
-function addElement(name) {
+function addElement(name, isSelection) {
+    console.log(isSelection);
     const recipe = recipes.find((e) => e.name == name);
-    const found = selection.find((e) => e.recipe.name == recipe.name);
+
+    let arrayToUpdate = selection;
+    if (!isSelection) {
+        arrayToUpdate = inventory;
+    }
+
+    const found = arrayToUpdate.find((e) => e.recipe.name == recipe.name);
     if (found) {
         found.amount += 1;
     } else {
-        selection.push({
+        arrayToUpdate.push({
             amount: 1,
             recipe: recipe,
         });
@@ -212,7 +284,11 @@ function addElement(name) {
 }
 
 function makeElement(recipe) {
-    return "<div class='recipe-element'><button onClick='addElement(\"" + recipe.name + "\")'>Add</button>" + recipe.name + "</div>";
+    return "<div class='recipe-element'>" +
+        "<button onClick='addElement(\"" + recipe.name + "\", true)'>Add To Plan</button>" + 
+        "<button onClick='addElement(\"" + recipe.name + "\", false)'>Add To Inventory</button>" + 
+        recipe.name + 
+        "</div>";
 }
 
 function makeSearchContents(filter) {
